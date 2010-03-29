d69d3ed0f1744a3290969e8623e606b29db768a6
Daniel Morris <daniel.john.morris@googlemail.com>
2010-03-29 14:46:05 +0100

    mock interface, clickable smiley buttons

--- UploadTutorial/Classes/UploadTutorialViewController.h
+++ UploadTutorial/Classes/UploadTutorialViewController.h
@@ -4,7 +4,23 @@
 @interface UploadTutorialViewController : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate> {
     IBOutlet UIImageView *imageView;
 		IBOutlet UIImageView *smileyView;
+		IBOutlet UITextField	*commentView;
+		IBOutlet UIButton *getPhotoButton;
+		IBOutlet UIButton *uploadPhotoButton;
+	NSString *smileyMood;
 }
-- (IBAction)pushPick:(id)sender;
-- (IBAction)pushUpload:(id)sender;
+
+-(IBAction) getPhoto:(id) sender;
+-(IBAction) sendData:(id) sender;
+
+-(IBAction) moodHappy:(id) sender;
+-(IBAction) moodAngry:(id) sender;
+
+@property (nonatomic, retain) IBOutlet UIImageView *imageView;;
+@property (nonatomic, retain) IBOutlet UIImageView *smileyView;
+@property (nonatomic, retain) IBOutlet UITextField *commentView;
+@property (nonatomic, retain) IBOutlet UIButton *getPhotoButton;
+@property (nonatomic, retain) IBOutlet UIButton *uploadPhotoButton;
+@property (nonatomic, retain) NSString *smileyMood;
+
 @end
--- UploadTutorial/Classes/UploadTutorialViewController.m
+++ UploadTutorial/Classes/UploadTutorialViewController.m
@@ -1,26 +1,89 @@
 #import "UploadTutorialViewController.h"
+//#import "EPUploader.h"
 
 @implementation UploadTutorialViewController
 
-- (IBAction)pushPick:(id)sender {
-	UIImagePickerController *picker = [[UIImagePickerController alloc] init];
-	picker.delegate = self;
-	picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
-	[self presentModalViewController:picker	animated:YES];
-	[picker release];
+@synthesize imageView;;
+@synthesize smileyView;
+@synthesize commentView;
+@synthesize getPhotoButton;
+@synthesize uploadPhotoButton;
+@synthesize smileyMood;
+
+-(IBAction) moodHappy:(id) sender {
+	NSLog(@"Happy");
+	smileyView.image = [UIImage imageNamed:@"happy.jpg"];
+	smileyMood = @"happy";
 }
 
-- (IBAction)pushUpload:(id)sender {
+-(IBAction) moodAngry:(id) sender {
+	NSLog(@"Angry");
+	smileyView.image = [UIImage imageNamed:@"angry.png"];
+	smileyMood = @"angry";
 }
 
-- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingImage:(UIImage *)image editingInfo:(NSDictionary *)editingInfo {
-	[picker.parentViewController dismissModalViewControllerAnimated:YES];
-	imageView.image = image;
-	smileyView.hidden = NO;
+- (IBAction)sendData:(id) sender {
+	/*
+	 turning the image into a NSData object
+	 getting the image back out of the UIImageView
+	 setting the quality to 90
+	 */
+	NSData *imageData = UIImageJPEGRepresentation(imageView.image, 90);
+	// setting up the URL to post to
+	NSString *urlString = @"http://danieljohnmorris.co.uk/thatrocks/upload.php";
+	
+	// setting up the request object now
+	NSMutableURLRequest *request = [[[NSMutableURLRequest alloc] init] autorelease];
+	[request setURL:[NSURL URLWithString:urlString]];
+	[request setHTTPMethod:@"POST"];
+	
+	/*
+	 add some header info now
+	 we always need a boundary when we post a file
+	 also we need to set the content type
+	 
+	 You might want to generate a random boundary.. this is just the same
+	 as my output from wireshark on a valid html post
+	 */
+	NSString *boundary = [NSString stringWithString:@"---------------------------14737809831466499882746641449"];
+	NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@",boundary];
+	[request addValue:contentType forHTTPHeaderField: @"Content-Type"];
+	
+	/*
+	 now lets create the body of the post
+	 */
+	NSMutableData *body = [NSMutableData data];
+	[body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+	[body appendData:[[NSString stringWithString:@"Content-Disposition: form-data; name=\"userfile\"; filename=\"ipodfile.jpg\"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+	[body appendData:[[NSString stringWithString:@"Content-Type: application/octet-stream\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+	[body appendData:[NSData dataWithData:imageData]];
+	[body appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+	// setting the body of the post to the reqeust
+	[request setHTTPBody:body];
+	
+	// now lets make the connection to the web
+	NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+	NSString *returnString = [[NSString alloc] initWithData:returnData encoding:NSUTF8StringEncoding];
+	
+	NSLog(returnString);
 }
 
-- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
-	[picker.parentViewController dismissModalViewControllerAnimated:YES];
+-(IBAction) getPhoto:(id) sender {
+	UIImagePickerController * picker = [[UIImagePickerController alloc] init];
+	picker.delegate = self; 
+	
+	if((UIButton *) sender == getPhotoButton) {
+		picker.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+	} else {
+		picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+	}
+	
+	[self presentModalViewController:picker animated:YES];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+	[picker dismissModalViewControllerAnimated:YES];
+	imageView.image = [info objectForKey:@"UIImagePickerControllerOriginalImage"];
 }
 
 @end

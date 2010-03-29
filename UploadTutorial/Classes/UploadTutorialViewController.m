4411c91164562a27cda6dbbe2285285cccfee87c
Daniel Morris <daniel.john.morris@googlemail.com>
2010-03-29 19:04:33 +0100

    managed object trying to write to filesystem but not saving :(

--- UploadTutorial/Classes/UploadTutorialViewController.m
+++ UploadTutorial/Classes/UploadTutorialViewController.m
@@ -1,5 +1,7 @@
 #import "UploadTutorialViewController.h"
 #import "UploadTutorialAppDelegate.h"
+//#import "UIImage+Resize.m"
+
 
 //#import "EPUploader.h"
 
@@ -51,6 +53,7 @@
 - (IBAction)sendData:(id) sender {
 		NSLog(@"initialising send");
 	
+	
 		UploadTutorialAppDelegate *appDelegate = (UploadTutorialAppDelegate *)[[UIApplication sharedApplication] delegate];
 //		NSString *urlString = @"http://192.168.1.79:3000/";
 		NSString *urlString = @"http://danieljohnmorris.co.uk/thatrocks/upload.php";
@@ -62,9 +65,31 @@
 		[request setHTTPMethod:@"POST"];
 		[request addValue:contentType forHTTPHeaderField: @"Content-Type"];
 		
-		NSString *photoPath = [[NSBundle mainBundle] pathForResource:@"chosen" ofType:@"jpg"];
-		NSData *photoData = [NSData dataWithContentsOfFile:photoPath];
-		
+
+	
+	
+	
+//	// scale to aspect fill
+//	CGSize size = CGSizeMake(320,480);
+//	UIImage *scaledImage = [imageView.image resizedImageWithContentMode: UIViewContentModeScaleAspectFill	bounds: size	interpolationQuality:kCGInterpolationDefault];
+//	
+//	// crop it centered
+//	int extraX = (int) (scaledImage.size.width - size.width);
+//	int extraY = (int) (scaledImage.size.height - size.height);
+//	CGRect cropRect = CGRectMake(extraX/2,extraY/2,size.width,size.height);
+//	UIImage *croppedImage = [scaledImage croppedImage: cropRect];
+//
+//	//		NSString *photoPath = [[NSBundle mainBundle] pathForResource:@"chosen" ofType:@"jpg"];
+//	//		NSData *photoData = [NSData dataWithContentsOfFile:photoPath];
+//	NSData *photoData = UIImageJPEGRepresentation(croppedImage, 60);
+	
+	NSData *photoData = UIImageJPEGRepresentation(imageView.image, 50);
+	
+	
+	
+	// croppedImage will be correctly scaled and cropped to the center
+	
+
 		NSMutableData *body = [NSMutableData data];
 
 	[body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
@@ -105,15 +130,30 @@
 }
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
-	NSData *imageData = UIImageJPEGRepresentation(imageView.image, 90);
-
+	// Create a file name for the image
+	NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+	[dateFormatter setTimeStyle:NSDateFormatterShortStyle];
+	[dateFormatter setDateStyle:NSDateFormatterShortStyle];
+	NSString *imageName = [NSString stringWithFormat:@"photo-%@.png",
+												 [dateFormatter stringFromDate:[NSDate date]]];
+	[dateFormatter release];	
+	
+	// Find the path to the documents directory
 	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
 	NSString *documentsDirectory = [paths objectAtIndex:0];
-	NSString *path = [documentsDirectory stringByAppendingPathComponent:@"chosen.jpg"];
 	
-	[imageData writeToFile:path atomically:YES];
-	[picker dismissModalViewControllerAnimated:YES];
-	imageView.image = [info objectForKey:@"UIImagePickerControllerOriginalImage"];
+	// Now we get the full path to the file
+	NSString *fullPathToFile = [documentsDirectory stringByAppendingPathComponent:imageName];
+	
+	// Write out the data.
+	[imageData writeToFile:fullPathToFile atomically:NO];
+	
+	// Set the managedObject's imageLocation attribute and save the managed object context
+	[self.managedObject setValue:fullPathToFile forKey:@"imagePath"];
+	NSError *error = nil;
+	[[self.managedObject managedObjectContext] save:&error];
+	
+	[self dismissModalViewControllerAnimated:YES];
 }
 
 
